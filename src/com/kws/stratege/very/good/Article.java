9043228cdd33cd92130b7550289ff7f2179cbf99
Park Min Kyu <niee@naver.com>
2016-03-05 11:45:08 +0900

    샘플 추가

---
+++ src/com/kws/stratege/very/good/Article.java
@@ -0,0 +1,28 @@
+package com.kws.stratege.very.good;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kws.stratege.good.service.ArticleDML;
+
+public abstract class Article {
+	
+	private ArticleDML articleDML;
+	
+	protected void setArticleDML(ArticleDML articleDML){
+		this.articleDML = articleDML;
+	}
+	
+	public boolean insertArticle(){
+		return articleDML.insertArticle();
+	}
+	public boolean updateArticle(){
+		return articleDML.updateArticle();
+	}
+	public boolean deleteArticle(){
+		return articleDML.deleteArticle();
+	}
+	
+	public abstract List selectArticle();
+	
+}
